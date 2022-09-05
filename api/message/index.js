9d349ef10ddd0f38a08267fed108fa4b2d178627
L_Beno <benoit@ahgency.be>
2022-09-05 12:28:27 +0200

    test connexion Db

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,17 @@
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
 
 module.exports = async function (context, req) {
+
+    connectionString.connect(function(err){
+        if (err) throw err;
+      
+        connectionString.query("SELECT * FROM USERS", function(err,result){
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello" + users + "from the API"
     });
 };
