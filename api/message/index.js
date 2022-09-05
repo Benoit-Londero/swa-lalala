d8be6a6c600796130817eda6dd66715ffaedd0e6
L_Beno <benoit@ahgency.be>
2022-09-05 16:23:20 +0200

    aaaaa

--- api/message/index.js
+++ api/message/index.js
@@ -1,12 +1,7 @@
 const conn = process.env.DATABASE_CONNECTION_STRING;
-
-con.connect(function(err){
-    if (err) throw err;
-    console.log('connect')
-});
     
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API" + conn
+        text: "Hello from the API"
     });
 };
